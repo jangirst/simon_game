4a4b6e4ca80b2b7a40425f0112d1e6055119207c
JanBoeker <naj_b@gmx.de>
2020-07-23 00:05:16 +0200

    finished game, like in the sample

--- game.js
+++ game.js
@@ -6,7 +6,10 @@ yellow = new Audio("sounds/yellow.mp3");
 
 var levelArray = [];
 var levelNumber = 0;
+var sequenceCounter = 0;
 var i = 0; // loopThrouSequence-Index
+
+alert("Each round a new color is added to the sequence. To finish the level, the complete sequence must be played each time. The game is finished as soon as a color is named incorrectly. Let's go!");
 
 // --------- document eventListener ---------
 $(document).keydown(function() {
@@ -14,42 +17,92 @@ $(document).keydown(function() {
   $(document).unbind("keydown");
 });
 
-$(document).click(function() {
-  nextLevel();
-});
-
 // --------- button eventListeners ---------
-$("#blue").click(function() {
-  blue.play();
-  pressButton("blue");
-});
-
 $("#green").click(function() {
-  green.play();
-  pressButton("green");
+
+  if (levelArray[sequenceCounter] === 0) {
+    green.play();
+    animateButton("green");
+    sequenceCounter += 1;
+
+    if (sequenceCounter === levelArray.length) {
+      nextLevel();
+    }
+
+  } else {
+    wrong.play();
+    animateButton("green");
+    gameOver();
+  }
+
 });
 
 $("#red").click(function() {
-  red.play();
-  pressButton("red");
+  if (levelArray[sequenceCounter] === 1) {
+    red.play();
+    animateButton("red");
+    sequenceCounter += 1;
+
+    if (sequenceCounter === levelArray.length) {
+      nextLevel();
+    }
+
+  } else {
+    wrong.play();
+    animateButton("red");
+    gameOver();
+  }
 });
 
 $("#yellow").click(function() {
-  yellow.play();
-  pressButton("yellow");
+  if (levelArray[sequenceCounter] === 2) {
+    yellow.play();
+    animateButton("yellow");
+    sequenceCounter += 1;
+
+    if (sequenceCounter === levelArray.length) {
+      nextLevel();
+    }
+
+  } else {
+    wrong.play();
+    animateButton("yellow");
+    gameOver();
+  }
 });
 
+$("#blue").click(function() {
+  if (levelArray[sequenceCounter] === 3) {
+    blue.play();
+    animateButton("blue");
+    sequenceCounter += 1;
+
+    if (sequenceCounter === levelArray.length) {
+      nextLevel();
+    }
+
+  } else {
+    wrong.play();
+    animateButton("blue");
+    gameOver();
+  }
+
+});
 
 // --------- functions ---------
-function pressButton(color) {
+function animateButton(color) {
+
+  // add CSS class for button down
   $("#" + color).addClass("pressed");
 
+  // remove CSS class for button down
   setTimeout(function() {
     $("#" + color).removeClass("pressed");
   }, 100);
 }
 
 function nextLevel() {
+
   // increase level
   levelNumber += 1;
 
@@ -59,49 +112,70 @@ function nextLevel() {
   // add new buttonNumber to sequence
   levelArray.push(nextButtonNumber);
 
-  i = 0
-  loopThrouSequence();
+  //reset loop-index and show actual sequence with loop
+  // i = 0
+  // loopThrouSequence();
 
+  setTimeout(function(){
+    animateActualSequence(nextButtonNumber);
+  }, 1000);
+
+
+  // update h1
   $("#level-title").text("Level " + levelNumber);
 
-  if (levelNumber === 10) {
-    gameOver();
-    $(document).bind("keydown", function() {
-      $("#level-title").text("Level 1");
-      $(document).unbind("keydown");
-    });
-  }
+  // reset sequenceCounter
+  sequenceCounter = 0;
+
 }
 
 function gameOver() {
+  // add CSS class for button down
+  $("body").addClass("game-over");
+
+  // remove CSS class for button down
+  setTimeout(function() {
+    $("body").addClass("game-over");
+  }, 50);
+
   // reset level
   levelNumber = 0;
+  levelArray = [];
+  sequenceCounter = 0;
 
   // update h1
   $("#level-title").text("Game Over, Press Any Key to Restart");
+
+  // add keydown eventListener again to document
+  $(document).bind("keydown", function() {
+    $("body").removeClass("game-over");
+    nextLevel();
+    $(document).unbind("keydown");
+  });
 }
 
 function animateActualSequence(buttonNumber) {
 
-  // just for testing
-  console.log(buttonNumber);
-
   switch (buttonNumber) {
     case 0:
+      console.log("green button");
       green.play();
-      pressButton("green");
+      animateButton("green");
       break;
     case 1:
+      console.log("red button");
       red.play();
-      pressButton("red");
+      animateButton("red");
       break;
     case 2:
+      console.log("yellow button");
       yellow.play();
-      pressButton("yellow");
+      animateButton("yellow");
       break;
     case 3:
+      console.log("blue button");
       blue.play();
-      pressButton("blue");
+      animateButton("blue");
       break;
     default:
       console.log("something went wrong in animateActualSequence()");
@@ -109,12 +183,13 @@ function animateActualSequence(buttonNumber) {
   }
 }
 
-function loopThrouSequence() {
-  setTimeout(function() {
-    animateActualSequence(levelArray[i]);
-    i++;
-    if (i < levelArray.length) {
-      loopThrouSequence();
-    }
-  }, 500)
-}
+// function loopThrouSequence() {
+//
+//   setTimeout(function() {
+//     animateActualSequence(levelArray[i]);
+//     i++;
+//     if (i < levelArray.length) {
+//       loopThrouSequence();
+//     }
+//   }, 500)
+// }
